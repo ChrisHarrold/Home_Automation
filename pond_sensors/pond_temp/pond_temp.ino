64409a476d05d0fb64a89ad4ddfeff3bf5bf8adf
charrold303 <christophereharrold@hotmail.com>
2020-09-04 11:15:45 +0200

    first update with mqtt code

--- pond_sensors/pond_temp/pond_temp.ino
+++ pond_sensors/pond_temp/pond_temp.ino
@@ -1,5 +1,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <time.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -12,11 +15,93 @@ DallasTemperature sensors(&oneWire);
 
 int deviceCount = 0;
 float tempC;
+float tempF;
+char data1[50];
+char data0[50];
+int placeholder_value;
+
+
+// declare our Wifi and MQTT connections and other constant settings for the network
+const char* ssid     = "International_House_of_Corgi_24";            // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "ElwoodIsBigAndFat";                          // The password of the Wi-Fi network
+const char* mqtt_server = "192.168.2.41";                             // The target mqtt server
+String clientId = "PT_1";
+int lcount = 0;
+
+// declare our Wifi and MQTT connections
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// setup the MCP3008 using default pins
+Adafruit_MCP3008 adc;
+
+//
+// Reconnects to the MQTT message-bus if the connection died, or we're
+// not otherwise connected.
+//
+void reconnectMQ() {
+
+  digitalWrite(net, LOW);  // turn the Blue NET LED Off
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wifi not connected - configuring");
+    setup_wifi();
+  } else {
+    Serial.println("Wifi Connected - connecting MQTT");
+  }
+
+  // Attempt to connect
+  Serial.println("Attempting to connect to MQTT Server...");
+  while (!client.connected()) {
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected to MQTT server");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+
+  // Once connected, publish an announcement...
+  String payload = "{\"Unit\":\"PT_1\", \"MQTT\":\"Connected\"}";
+  payload.toCharArray(data1, (payload.length() + 1));
+  client.publish("control", data1); //the "control" topic is just for notifications - change to fit your needs
+
+  digitalWrite(net, HIGH);  // turn the Blue NET LED on
+  delay(5000);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  WiFi.begin(ssid, password);             // Connect to the wifi network
+
+  while (WiFi.status() != WL_CONNECTED) // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print('.');
+
+  Serial.println('\n');
+  Serial.println("Connected!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP32 to the serial port
+  
+}
 
 void setup(void)
 {
   sensors.begin();	// Start up the library
   Serial.begin(9600);
+  Serial.println("Starting up");
+  
+  // Define the MQTT Server connection settings and then launch the MQTT Connection
+  client.setServer(mqtt_server, 1883);
+
+  //Connect to the wireless network
+  setup_wifi();
   
   // locate devices on the bus
   Serial.print("Locating devices...");
@@ -42,11 +127,22 @@ void loop(void)
     Serial.print(tempC);
     Serial.print((char)176);//shows degrees character
     Serial.print("C  |  ");
-    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    tempF=(DallasTemperature::toFahrenheit(tempC));
+    Serial.print(tempF);
     Serial.print((char)176);//shows degrees character
     Serial.println("F");
   }
   
+  if (!client.connected()) {
+    reconnectMQ();
+  }
+  
+  placeholder_value=sprintf(data0, "{\"Message\":\"\", \"Sensors\": {\"C_Temp\":\"%s\", \"F_temp\":\"%s\"}}", tempC, tempF);
+  Serial.println("Publishing message");
+  while (!client.publish("PT_1", data0)) {
+    Serial.print(".");
+  }
+
   Serial.println("");
-  delay(1000);
+  delay(10000);
 }
