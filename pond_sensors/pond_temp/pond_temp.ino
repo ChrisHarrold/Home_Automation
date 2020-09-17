25df65540d6c23c5d884f095147f889e142cd19d
charrold303 <christophereharrold@hotmail.com>
2020-09-17 11:59:17 +0200

    fixing 1 hour deep sleep issues

--- pond_sensors/pond_temp/pond_temp.ino
+++ pond_sensors/pond_temp/pond_temp.ino
@@ -5,6 +5,18 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+// Set RTC Memory Values and info for deep-sleep counter
+// Deep sleep taps out at 71 minutes, so for 4 hours you need to sleep
+// 1 hour, looped 4 times
+#define RTCMEMORYSTART 65
+#define MAXHOUR 4 // number of hours to deep sleep for
+typedef struct {
+  int count;
+} rtcStore;
+
+rtcStore rtcMem;
+
+
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS D1
 
@@ -88,9 +100,28 @@ void setup_wifi() {
 
 void setup(void)
 {
-  sensors.begin();	// Start up the library
   Serial.begin(9600);
   Serial.println("Starting up");
+  // First thing to do is check the RTC memory and increment if we have not hit
+  // our full sleep interval - this will prevent all the WIFI stuff from running
+  // and adding battery drain when it isn't time to sample. I will remove the 
+  // serial outputs, post testing to save a couple more mA as well
+
+  Serial.println("Reading RTC Value");
+  readFromRTCMemory();
+  Serial.println("Updating RTC Value");
+  writeToRTCMemory();
+
+  Serial.println("Checking sleep status");
+  if (rtcMem.count != 0) {
+    Serial.println("Going back to sleep for an hour - will try again");
+    ESP.deepSleep(3,6e+9);
+  }
+  
+  // If the RTC mem check passes, the program will sample the temps
+  
+  sensors.begin();	// Start up the library
+
   
   // Define the MQTT Server connection settings and then launch the MQTT Connection
   client.setServer(mqtt_server, 1883);
@@ -137,8 +168,10 @@ void loop(void)
       placeholder_value=sprintf(data0, "{\"Sensor\":\"%d\", \"Values\": {\"C_Temp\":\"%.2f\", \"F_temp\":\"%.2f\"}}", devs, tempC, tempF);
       //Serial.println("Publishing message");
       while (!client.publish("Pond", data0)) {
-      Serial.print(".");
+        Serial.print(".");
       }
+      // this is going to look really weird to you. Trust me, it works. Why? Probably because the MQTT library is FULL of weird
+      // conflicting varible things and has a nasty tendency to break stuff like this. Just leave this here, it works.
       sensor_count = sensors.getDeviceCount();
     }
 
@@ -147,6 +180,32 @@ void loop(void)
   //Serial.println("");
   client.publish("control", "{\"Unit\":\"PT_1\", \"Power\":\"Preparing to deep sleep\"}"); //the "control" topic is just for notifications - change to fit your needs
   delay(10000);
-  ESP.deepSleep(30e6);  
-  //delay(30000);
+  
+  // now we restart the sleep cycle after taking all samples
+  // nothing runs after this
+  ESP.deepSleep(3,6e+9);
 }
+
+void readFromRTCMemory() {
+  system_rtc_mem_read(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
+
+  Serial.print("count = ");
+  Serial.println(rtcMem.count);
+  yield();
+}
+
+void writeToRTCMemory() {
+  if (rtcMem.count == MAXHOUR) {
+    rtcMem.count = 0;
+  } else {
+    rtcMem.count++;
+  }
+
+  system_rtc_mem_write(RTCMEMORYSTART, &rtcMem, 4);
+
+  Serial.print("Current loop count = ");
+  Serial.println(rtcMem.count);
+  yield();
+}
+
+
