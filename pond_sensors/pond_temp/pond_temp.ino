34abd1d17808ca15594c9919e5465dcf5aa88bb4
charrold303 <christophereharrold@hotmail.com>
2020-10-02 18:33:15 +0200

    added DHT11. Functions for reads. override pin configured

--- pond_sensors/pond_temp/pond_temp.ino
+++ pond_sensors/pond_temp/pond_temp.ino
@@ -1,3 +1,4 @@
+#include <DHT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <time.h>
@@ -16,22 +17,32 @@ typedef struct {
 
 rtcStore rtcMem;
 
-
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS D1
 
+// DHT 11 Settings
+#define DHTPIN D2
+#define DHTTYPE DHT11
+
+
 // Setup a oneWire instance to communicate with any OneWire device
-OneWire oneWire(ONE_WIRE_BUS);	
+OneWire oneWire(ONE_WIRE_BUS);
+
+//Setup DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
+// other variables
+int override_pin = D3;
 int sensor_count = 0;
 float tempC;
 float tempF;
 char data1[50];
 char data0[50];
 int placeholder_value;
+bool debug_mode = false;
 
 
 // declare our Wifi and MQTT connections and other constant settings for the network
@@ -45,9 +56,148 @@ int lcount = 0;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Reconnects to the MQTT message-bus if the connection died, or we're
-// not otherwise connected.
-//
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("Starting up");
+  // First thing to do is check the RTC memory and increment if we have not hit
+  // our full sleep interval - this will prevent all the WIFI stuff from running
+  // and adding battery drain when it isn't time to sample. I will remove the 
+  // serial outputs, post testing to save a couple more mA as well
+
+  // CHeck the overide pin to see if we are in programming/debug or runtime mode (DIP 2 connected to D3)
+  pinMode(override_pin, INPUT);;
+  if (digitalRead(override_pin) == HIGH) {
+    debug_mode = true;
+  }
+  
+  if (debug_mode = false) {
+    Serial.println("Reading RTC Value");
+    readFromRTCMemory();
+    Serial.println("Updating RTC Value");
+    writeToRTCMemory();
+
+    Serial.println("Checking sleep status");
+    if (rtcMem.count != 1) {
+      Serial.println("Going back to sleep for an hour - will try again");
+      ESP.deepSleep(3.6e+9);
+    }
+  }
+  // If the RTC mem check passes, the program will sample the temps
+  sensors.begin();	// Start up the onewire sensors
+  dht.begin();      // start up the DHT11
+
+  // Define the MQTT Server connection settings and then launch the MQTT Connection
+  client.setServer(mqtt_server, 1883);
+
+  //Connect to the wireless network
+  setup_wifi();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  sensor_count = sensors.getDeviceCount();
+  Serial.print(sensor_count, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+}
+
+void loop(void)
+{ 
+  //reconnect mqtt at loop start  
+    if (!client.connected()) {
+      reconnectMQ();
+  }
+
+  one_wire_read();
+  dht_read();
+
+
+  //Serial.println("And Now We Wait");
+  //Serial.println("");
+  client.publish("control", "{\"Unit\":\"PT_1\", \"Power\":\"Preparing to deep sleep\"}"); //the "control" topic is just for notifications - change to fit your needs
+  delay(10000);
+  
+  // now we restart the sleep cycle after taking all samples
+  // nothing runs after this
+  if (debug_mode = false) {
+    ESP.deepSleep(3.6e+9);
+  } else {
+    delay(30000);
+    ESP.restart;
+  }
+  
+}
+
+void readFromRTCMemory() {
+  system_rtc_mem_read(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
+
+  Serial.print("count = ");
+  Serial.println(rtcMem.count);
+  yield();
+}
+
+void dht_read() {
+  //DHT11 read and collect variables - float is a numeric type with better precision than int
+  float humidity = dht.readHumidity();
+  // Read temperature as Celsius (the default - can be change to Farenheit if desired)
+  float temperature = dht.readTemperature();
+  // Check if any reads failed and if so alert to the console, 
+  // and set values to 0 so the process can continue - DHT11 sensors are notoriously flaky
+  if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor! Values set to 0!");
+      temperature = 0;
+      humidity = 0;
+  //actually got a clean reading so go down the happy path and format the reading for the MQTT message    
+  } else {
+      Serial.println(" DHT11 Readings: ");
+      Serial.print("Humidity: "); Serial.print(humidity); Serial.print(" %\t"); 
+      Serial.print("Temperature: "); Serial.print(temperature); 
+      Serial.println(" *C ");
+  }
+  //Serial.println("Constructing the DHT11 payload:");
+  placeholder_value=sprintf(data0, "{\"Sensor\":\"DHT-11\", \"Values\": {\"C_Temp\":\"%.2f\", \"Humidity\":\"%.2f\"}}", temperature, humidity);
+  //Serial.println("Publishing the DHT11 message");
+  while (!client.publish("Pond", data0)) {
+    Serial.print(".");
+  }
+}
+
+void one_wire_read() {
+
+  // Send command to all the sensors for temperature conversion
+  sensors.requestTemperatures(); 
+  
+  // Display temperature from each sensor
+  for (int devs = 0;  devs < sensor_count;  devs++)
+  {
+    Serial.print("Sensor ");
+    Serial.print(devs+1);
+    Serial.print(" : ");
+    tempC = sensors.getTempCByIndex(devs);
+    Serial.print(tempC);
+    Serial.print((char)176);//shows degrees character
+    Serial.print("C  |  ");
+    tempF=(DallasTemperature::toFahrenheit(tempC));
+    Serial.print(tempF);
+    Serial.print((char)176);//shows degrees character
+    Serial.println("F");
+    
+    if (devs < sensor_count) {
+      //Serial.println("Constructing the payload:");
+      placeholder_value=sprintf(data0, "{\"Sensor\":\"%d\", \"Values\": {\"C_Temp\":\"%.2f\", \"F_temp\":\"%.2f\"}}", devs, tempC, tempF);
+      //Serial.println("Publishing message");
+      while (!client.publish("Pond", data0)) {
+        Serial.print(".");
+      }
+      // this is going to look really weird to you. Trust me, it works. Why? Probably because the MQTT library is FULL of weird
+      // conflicting varible things and has a nasty tendency to break stuff like this. Just leave this here, it works.
+      sensor_count = sensors.getDeviceCount();
+    }
+
+  }
+
+}
 
 void reconnectMQ() {
 
@@ -98,108 +248,13 @@ void setup_wifi() {
   
 }
 
-void setup(void)
-{
-  Serial.begin(9600);
-  Serial.println("Starting up");
-  // First thing to do is check the RTC memory and increment if we have not hit
-  // our full sleep interval - this will prevent all the WIFI stuff from running
-  // and adding battery drain when it isn't time to sample. I will remove the 
-  // serial outputs, post testing to save a couple more mA as well
-
-  Serial.println("Reading RTC Value");
-  readFromRTCMemory();
-  Serial.println("Updating RTC Value");
-  writeToRTCMemory();
-
-  Serial.println("Checking sleep status");
-  if (rtcMem.count != 0) {
-    Serial.println("Going back to sleep for an hour - will try again");
-    ESP.deepSleep(3.6e+9);
-  }
-  
-  // If the RTC mem check passes, the program will sample the temps
-  
-  sensors.begin();	// Start up the library
-
-  
-  // Define the MQTT Server connection settings and then launch the MQTT Connection
-  client.setServer(mqtt_server, 1883);
-
-  //Connect to the wireless network
-  setup_wifi();
-  
-  // locate devices on the bus
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  sensor_count = sensors.getDeviceCount();
-  Serial.print(sensor_count, DEC);
-  Serial.println(" devices.");
-  Serial.println("");
-}
-
-void loop(void)
-{ 
-  //reconnect mqtt at loop start  
-    if (!client.connected()) {
-      reconnectMQ();
-  }
-
-  // Send command to all the sensors for temperature conversion
-  sensors.requestTemperatures(); 
-  
-  // Display temperature from each sensor
-  for (int devs = 0;  devs < sensor_count;  devs++)
-  {
-    Serial.print("Sensor ");
-    Serial.print(devs+1);
-    Serial.print(" : ");
-    tempC = sensors.getTempCByIndex(devs);
-    Serial.print(tempC);
-    Serial.print((char)176);//shows degrees character
-    Serial.print("C  |  ");
-    tempF=(DallasTemperature::toFahrenheit(tempC));
-    Serial.print(tempF);
-    Serial.print((char)176);//shows degrees character
-    Serial.println("F");
-    
-    if (devs < sensor_count) {
-      //Serial.println("Constructing the payload:");
-      placeholder_value=sprintf(data0, "{\"Sensor\":\"%d\", \"Values\": {\"C_Temp\":\"%.2f\", \"F_temp\":\"%.2f\"}}", devs, tempC, tempF);
-      //Serial.println("Publishing message");
-      while (!client.publish("Pond", data0)) {
-        Serial.print(".");
-      }
-      // this is going to look really weird to you. Trust me, it works. Why? Probably because the MQTT library is FULL of weird
-      // conflicting varible things and has a nasty tendency to break stuff like this. Just leave this here, it works.
-      sensor_count = sensors.getDeviceCount();
-    }
-
-  }
-  //Serial.println("And Now We Wait");
-  //Serial.println("");
-  client.publish("control", "{\"Unit\":\"PT_1\", \"Power\":\"Preparing to deep sleep\"}"); //the "control" topic is just for notifications - change to fit your needs
-  delay(10000);
-  
-  // now we restart the sleep cycle after taking all samples
-  // nothing runs after this
-  ESP.deepSleep(3.6e+9);
-}
-
-void readFromRTCMemory() {
-  system_rtc_mem_read(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
-
-  Serial.print("count = ");
-  Serial.println(rtcMem.count);
-  yield();
-}
 
 void writeToRTCMemory() {
   if (rtcMem.count == MAXHOUR) {
-    rtcMem.count = 0;
+    rtcMem.count = 1;
   } else {
-    if (rtcMem.count < 0) {
-      rtcMem.count = 0;
+    if (rtcMem.count < 1) {
+      rtcMem.count = 1;
     }
     rtcMem.count++;
   }
