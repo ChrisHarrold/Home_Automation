b7577a99da041739575fd5d0613193fa55779af6
charrold303 <christophereharrold@hotmail.com>
2021-01-18 16:49:51 +0100

    Create flow_test.py

---
+++ filter_control/flow_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time, sys
+
+
+FLOW_SENSOR = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+global count
+count = 0
+
+def countPulse(channel):
+   global count
+   count = count+1
+   print "The current pulse count is {0}.".format (count)
+   print '\n'
+
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.BOTH, callback=countPulse)
+
+
+while True:
+    try:
+        #print(GPIO.input(23))
+	    #print (count)
+
+        time.sleep(10)
+	
+    except KeyboardInterrupt:
+        print '\ncaught keyboard interrupt!, bye'
+        GPIO.cleanup()
+        sys.exit()
