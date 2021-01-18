031496d36d161aa1c80f474e8cc763305ee0843f
charrold303 <christophereharrold@hotmail.com>
2021-01-18 18:12:00 +0100

    Update lcd_test.py

--- filter_control/lcd_test.py
+++ filter_control/lcd_test.py
@@ -34,7 +34,7 @@ while i > 0:
     # Clear the LCD screen
     #lcd.clear()
     lcd.home()
-    lcd.cursor_pos(4, 19)
+    lcd.cursor_pos = (4, 19)
     lcd.write_string('{}'.format(i))
     sleep(1)
 
