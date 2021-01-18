8e2b52adb82cddfb63240dbb86dd39fd3eb71e0d
charrold303 <christophereharrold@hotmail.com>
2021-01-18 18:08:05 +0100

    Update lcd_test.py

--- filter_control/lcd_test.py
+++ filter_control/lcd_test.py
@@ -11,7 +11,7 @@ cols = 20
 rows = 4
 charmap = 'A00'
 i2c_expander = 'PCF8574'
-i=1
+i=60
 current_flow_1 = 200
 current_flow_2 = 185
 
@@ -23,30 +23,20 @@ port = 1 # 0 on an older Raspberry Pi
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
 
-def write_to_lcd_display():
-    # Write a string on first line and move to next line
-    lcd.write_string('Tank 1: '+ str(current_flow_1) + ' LPM')
-    lcd.crlf()
-    lcd.crlf()
-    lcd.write_string('Tank 2: '+ str(current_flow_2) + ' LPM')
-    lcd.crlf()
-    now = datetime.datetime.now()
-    lcd.write_string('Update: ' + now.strftime("%H:%M:%S"))
-    sleep(10)
+lcd.clear()
+lcd.write_string('Flow on 1 here:')
+lcd.crlf()
+lcd.write_string('Flow on two here:')
+lcd.crlf()
+lcd.write_string('Next line is the test')
 
-while i < 100:
+while i > 0:
     # Clear the LCD screen
-    lcd.clear()
-    sleep(5)
-    write_to_lcd_display()
+    #lcd.clear()
+    lcd.cursor_pos(4,19)
+    lcd.write_string('{}'.format(i))
+    sleep(1)
+
 
 lcd.clear()
-lcd.write_string('Testing Complete')
-lcd.crlf()
-lcd.write_string('This Will remain on screen')
-lcd.crlf()
-lcd.write_string('I hope?')
-lcd.crlf()
-lcd.write_string('I wonder what happens...')
-
-sleep(30)
+lcd.close()
