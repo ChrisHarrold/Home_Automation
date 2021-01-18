ce14b6c642bbc3a7daa0a4821a4fb1094567277d
charrold303 <christophereharrold@hotmail.com>
2021-01-18 17:41:27 +0100

    Update flow_test.py

--- filter_control/flow_test.py
+++ filter_control/flow_test.py
@@ -4,26 +4,34 @@ import RPi.GPIO as GPIO
 import time, sys
 
 
-FLOW_SENSOR = 18
+FLOW_SENSOR1 = 18
+FLOW_SENSOR2 = 23
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-global count
-count = 0
+global count1
+global count2
+count1 = 0
+count2 = 0
 
-def countPulse(channel):
-   global count
-   count = count+1
+def Flow_meter1(channel):
+   global count1
+   count1 = count1+1
 
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
+def Flow_meter2(channel):
+   global count2
+   count2 = count2+1
+
+GPIO.add_event_detect(FLOW_SENSOR1, GPIO.FALLING, callback=Flow_meter1)
+GPIO.add_event_detect(FLOW_SENSOR2, GPIO.FALLING, callback=Flow_meter2)
 
 
 while True:
     try:
         #print(GPIO.input(23))
 	    #print (count)
-        print('The current pulse count is {0} '.format (count))
-        print('\n')
+        print('Flow Rate on tank 1 is: {0} \n'.format (count1))
+        print('Flow Rate on tank 2 is: {0} \n'.format (count2))
         time.sleep(10)
 	
     except KeyboardInterrupt:
