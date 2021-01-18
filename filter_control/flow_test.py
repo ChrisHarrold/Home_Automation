f4b3cee26d3c210754c6929c5e60efe4d129ff56
charrold303 <christophereharrold@hotmail.com>
2021-01-18 17:55:41 +0100

    Update flow_test.py

--- filter_control/flow_test.py
+++ filter_control/flow_test.py
@@ -13,7 +13,14 @@ GPIO.setup(FLOW_SENSOR2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 global count1
 global count2
 count1 = 0
+lastcount1 = 0
+current_count1 = 0
 count2 = 0
+lastcount2 = 0
+current_count2 = 0
+interval = 1
+flow1 = 0
+flow2 = 0
 
 def Flow_meter1(channel):
    global count1
@@ -29,11 +36,24 @@ GPIO.add_event_detect(FLOW_SENSOR2, GPIO.FALLING, callback=Flow_meter2)
 
 while True:
     try:
-        #print(GPIO.input(23))
-	    #print (count)
-        print('Flow Rate on tank 1 is: {0} \n'.format (count1))
-        print('Flow Rate on tank 2 is: {0} \n'.format (count2))
-        time.sleep(10)
+        while interval < 6:
+            print('Current loop is number {0}. No reading will be taken\n'.format(interval))
+            time.sleep(10)
+            interval = interval+1
+        else:
+            current_count1 = count1 - lastcount1
+            current_count2 = count2 - lastcount2
+            
+            flow1 = (current_count1/.55)
+            flow2 = (current_count2/.55)
+
+            print('Flow Rate on tank 1 is: {0} Liters/Minute\n'.format (flow1))
+            print('Flow Rate on tank 2 is: {0} Liters/Minute\n'.format (flow2))
+
+            lastcount1 = count1
+            lastcount2 = count2
+
+            
 	
     except KeyboardInterrupt:
         print('\ncaught keyboard interrupt!, bye')
