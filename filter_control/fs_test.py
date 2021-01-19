ff3e9b7c456636536fef0e1e20e4df5e9001bc06
charrold303 <christophereharrold@hotmail.com>
2021-01-19 13:48:20 +0100

    Update fs_test.py

--- filter_control/fs_test.py
+++ filter_control/fs_test.py
@@ -1,3 +1,3 @@
 import subprocess
-result = subprocess.run(['sudo', 'ls', '/sys/bus/w1/devices', '-d', '28*'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+result = subprocess.run(['sudo', 'ls', '/sys/bus/w1/devices/28*', '-d',], stdout=subprocess.PIPE).stdout.decode('utf-8')
 print (result)  
