d01868f6007b957f9ebaeef0ce267e6ad91afeee
charrold303 <christophereharrold@hotmail.com>
2021-01-19 13:54:32 +0100

    added

--- filter_control/fs_test.py
+++ filter_control/fs_test.py
@@ -1,3 +1,11 @@
-import subprocess
-result = subprocess.run(['sudo', 'ls', '/sys/bus/w1/devices/28*', '-d',], stdout=subprocess.PIPE).stdout.decode('utf-8')
-print (result)  
+# Temp.py
+
+from ds18b20 import DS18B20	
+	
+# test temperature sensors
+x = DS18B20()
+count=x.device_count()
+i = 0
+while i < count:
+	print(x.tempC(i))
+	i += 1
