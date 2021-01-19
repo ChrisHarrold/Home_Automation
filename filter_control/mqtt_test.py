f6da9a6c56fd7727d66e02b41735a883a19a9e76
charrold303 <christophereharrold@hotmail.com>
2021-01-19 16:45:30 +0100

    Update mqtt_test.py

--- filter_control/mqtt_test.py
+++ filter_control/mqtt_test.py
@@ -2,4 +2,4 @@ import paho.mqtt.client as mqtt #import the client1
 broker_address="192.168.68.115" 
 client = mqtt.Client("Filter_Monitor") #create new instance
 client.connect(broker_address) #connect to broker
-client.publish("TEST","TEST")#publish
+client.publish("Pond","TEST")#publish
