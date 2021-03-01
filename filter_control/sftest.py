0c61c3f24914edd55ace6c206f88fe0a72ee2e3c
charrold303 <christophereharrold@hotmail.com>
2021-03-01 12:17:14 +0100

    Create sftest.py

---
+++ filter_control/sftest.py
@@ -0,0 +1,17 @@
+# simple function test
+this_publish = 'This is the data string before'
+
+def create_data_to_publish () :
+    flow1 = 10
+    flow2 = 20
+    data0 = '{{\"Unit\":\"Filter\",\"Sensor\":\"Filter_Flow\",\"Values\":{{\"Flow1\":\"{0:.2f}\",\"Flow2\":\"{1:.2f}\"}}}}'.format (flow1, flow2)
+    return data0
+
+def publish_data (d1, d2) :
+    print(d1)
+    return d1
+
+this_publish = create_data_to_publish()
+data1 = publish_data(this_publish)
+
+print(data1)
