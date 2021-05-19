41574f70403b4b4c14a4d81fa04898718cb5c93d
charrold303 <christophereharrold@hotmail.com>
2021-05-19 17:26:33 +0200

    Update coop_controller.py

--- coop_control/coop_controller.py
+++ coop_control/coop_controller.py
@@ -2,5 +2,5 @@ from picamera import PiCamera
 import time
 
 coop_cam1 = PiCamera()
-coop_cam1.capture('/pictures/coop_pic.jpg')
+coop_cam1.capture('/var/www/html/coop_pic.jpg')
 print("took a pic!")
