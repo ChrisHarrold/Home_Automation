05d1b76d23d6445b57d2e7d8f5dba320ca09bd6d
charrold303 <christophereharrold@hotmail.com>
2021-05-20 13:12:36 +0200

    Update coop_controller.py

--- coop_control/coop_controller.py
+++ coop_control/coop_controller.py
@@ -7,6 +7,10 @@ from time import sleep
 coop_cam1 = PiCamera()
 client = mqtt.Client()
 
+# these are the pins that interface to the L298N driver board and tell it which way to spin
+openPin = 34
+closePin = 35
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("Door_Actions")
@@ -21,16 +25,34 @@ def on_message(client, userdata, msg):
         print("CLOSE!")
         # The door will close once I add the motor controlls here
         # it also needs to then reply with a message on the status of the door
-      
+
+        # turn on CLOSE pin
+
+        # sleep long enough to open door (some number of seconds)
+
+        # turn off close pin
+
+        # publish new door state message
+        client.publish("Door_Status", "CLOSED")
+
     elif (msg.payload == 'coop_open'):
         print("OPEN!")
         # The door will open once I add the motor controls here
         # it also needs to then reply with a message on the status of the door
 
+        # turn on OPEN pin
+
+        # sleep long enough to open door (some number of seconds)
+
+        # turn off open pin
+
+        # publish new door state message
+        client.publish("Door_Status", "OPEN")
+
 client.on_connect = on_connect
 client.on_message = on_message
-#Normally I wouldn't connect to the MQ server right away, but this device is going to be "always on" and needs to
-#be listening at all times to function
+#this device is going to be "always on" and needs to
+#be listening at all times to function so connect immediately
 client.connect("192.168.68.115",1883,60)
 client.loop_start()
 
@@ -38,10 +60,6 @@ while True:
     try:
         coop_cam1.capture('/var/www/html/coop_pic.jpg')
         print("took a pic!")
-        sleep(20)
-        client.publish("Door_Status", "OPEN")
-        sleep(20)
-        client.publish("Door_Status", "CLOSED")
         sleep(20)
 
     except KeyboardInterrupt:
