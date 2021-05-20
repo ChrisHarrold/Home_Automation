8ca080c5f2aed7a002efb3dd2b1898a7b3fcd156
charrold303 <christophereharrold@hotmail.com>
2021-05-20 13:33:52 +0200

    updates

--- coop_control/coop_controller.py
+++ coop_control/coop_controller.py
@@ -2,14 +2,24 @@ import sys, datetime, os
 from picamera import PiCamera
 import paho.mqtt.client as mqtt
 from time import sleep
+import RPi.GPIO as GPIO
+from ds18b20 import DS18B20
 
 #setup variables and values
 coop_cam1 = PiCamera()
 client = mqtt.Client()
 
 # these are the pins that interface to the L298N driver board and tell it which way to spin
-openPin = 34
-closePin = 35
+openPin = 5
+closePin = 6
+active_running_led = 13
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(active_running_led, GPIO.OUT, initial=1)
+GPIO.setup(openPin, GPIO.OUT)
+GPIO.setup(closePin, GPIO.OUT)
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -17,10 +27,10 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     payload = str(msg.payload.decode("utf-8"))
-    print("message received " ,payload)
-    print("message topic=",msg.topic)
-    print("message qos=",msg.qos)
-    print("message retain flag=",msg.retain)
+    #print("message received " ,payload)
+    #print("message topic=",msg.topic)
+    #print("message qos=",msg.qos)
+    #print("message retain flag=",msg.retain)
     
     if (payload == 'coop_close'):
         print("CLOSE!")
@@ -28,11 +38,11 @@ def on_message(client, userdata, msg):
         # it also needs to then reply with a message on the status of the door
 
         # turn on CLOSE pin
-
+        GPIO.output(closePin, 1)
         # sleep long enough to open door (some number of seconds)
-
+        sleep(10)
         # turn off close pin
-
+        GPIO.output(closePin, 0)
         # publish new door state message
         client.publish("Door_Status", "CLOSED")
 
@@ -42,11 +52,11 @@ def on_message(client, userdata, msg):
         # it also needs to then reply with a message on the status of the door
 
         # turn on OPEN pin
-
+        GPIO.output(openPin, 1)
         # sleep long enough to open door (some number of seconds)
-
+        sleep(10)
         # turn off open pin
-
+        GPIO.output(openPin, 0)
         # publish new door state message
         client.publish("Door_Status", "OPEN")
 
@@ -66,5 +76,7 @@ while True:
     except KeyboardInterrupt:
         client.disconnect()
         client.loop_stop()
+        GPIO.output(active_running_led, 0)
+        GPIO.cleanup()
         sys.exit()
 
