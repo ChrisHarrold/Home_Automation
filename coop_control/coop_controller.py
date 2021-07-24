b9d23c5d0f20ed0c58986cb1a65224ed8a8ed2b6
charrold303 <christophereharrold@hotmail.com>
2021-07-24 15:13:56 +0200

    Update coop_controller.py

--- coop_control/coop_controller.py
+++ coop_control/coop_controller.py
@@ -111,9 +111,9 @@ def Collect_Temp_Data() :
         #lcd.cursor_pos = (2,0)
         #lcd.write_string('{0:.1f}'.format (the_tempC[0]))
         if i > 0 :
-            data1 = ('{{\"Unit\":\"Coop\",\"Sensor\":\"Coop_Temp\",\"Values\":{{\"T1_C\":\"{0:.2f}\",\"T1_F\":\"{1:.2f}\"}}'.format (the_tempC[0], the_tempF[0]))
+            data1 = ('\"Unit\":\"Coop\",\"Sensor\":\"Coop_Temp\",\"Values\":{{\"T1_C\":\"{0:.2f}\",\"T1_F\":\"{1:.2f}\"}}'.format (the_tempC[0], the_tempF[0]))
         else :
-            data1 = ('\"Unit\":\"Coop\",\"Sensor\":\"Coop_Temp\",\"Values\":\"NO DATA\"')
+            data1 = ("\"Unit\":\"Coop\",\"Sensor\":\"Coop_Temp\",\"Values\":\"NO DATA\"")
         
         publish_message("Coop_Sensors", data1)
         return
