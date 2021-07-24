0fd20c06a9edf35a6bebaa4655b9e613d8b03f42
charrold303 <christophereharrold@hotmail.com>
2021-07-24 16:13:57 +0200

    Create led_blink.py

---
+++ coop_control/led_blink.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+blink_me = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(blink_me, GPIO.OUT)
+i=0
+
+while i < 100 :
+    GPIO.output(blink_me, GPIO.HIGH)
+    print("LED on")
+    time.sleep(5)
+    print("LED off")
+    GPIO.output(blink_me),GPIO.LOW)
+    i=i+1
+
+
